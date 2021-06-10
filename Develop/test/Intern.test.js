74eb17b126b8dcae3ba62c7641fbf7c625793b7a
Josh <jdogcrane@gmail.com>
2021-06-09 21:12:57 -0400

    intern class done and pass test

--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -3,13 +3,19 @@
 const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
-
+    const testName = "userName";
+    const emp = new Intern("laa", 1, "test@test.com", testName);
+    expect(emp.school).toBe(testName);
 });
 
 test("getRole() should return \"Intern\"", () => {
-
+    const testName = "Intern";
+    const emp = new Intern("laa", 1, "test@test.com", testName);
+    expect(emp.getRole()).toBe(testName);
 });
 
 test("Can get school via getSchool()", () => {
-
+    const testName = "userName";
+    const emp = new Intern("laa", 1, "test@test.com", testName);
+    expect(emp.getSchool()).toBe(testName);
 });
