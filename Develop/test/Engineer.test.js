4c30bfb698feff008f2b352bd60cd69bb2bd1334
Josh <jdogcrane@gmail.com>
2021-06-09 21:01:52 -0400

    eng class done and test passed

--- Develop/test/Engineer.test.js
+++ Develop/test/Engineer.test.js
@@ -3,13 +3,19 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
-
+const testEmail = "userName";
+const emp = new Engineer("laa", 1, "test@test.com", testEmail);
+expect(emp.github).toBe(testEmail);
 });
 
 test("getRole() should return \"Engineer\"", () => {
-
+    const testRole = "Engineer";
+    const emp = new Engineer("laa", 1, "test@test.com", testRole);
+    expect(emp.getRole()).toBe(testRole)
 });
 
 test("Can get GitHub username via getGithub()", () => {
-
+    const testGit = "userName";
+    const emp = new Engineer("laa", 1, "test@test.com", testGit);
+    expect(emp.getGithub()).toBe(testGit)
 });
